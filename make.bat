b9130bb02cfc9e9e60fe577573e17d1561903c25
Vopaaz <liyifan945@163.com>
2019-09-03 11:25:45 -0500

    Add error detection for makefile

--- make.bat
+++ make.bat
@@ -1,23 +1,25 @@
 @echo off
 
 set root=%cd%
+set silentfile=silent.aux
 
 cd %root%/en
-lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-EN-verbose.tex"
-lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-EN-verbose.tex"
-echo EN-verbose: FINISHED
+lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-EN-verbose.tex" > %silentfile%
+lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-EN-verbose.tex" > %silentfile%
+if errorlevel 1 ( echo [ATTENTION] EN-verbose: FAILED ) else ( echo EN-verbose: FINISHED )
 
 cd %root%/en
-lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-EN.tex"
-lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-EN.tex"
-echo EN: FINISHED
+lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-EN.tex" > %silentfile%
+lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-EN.tex" > %silentfile%
+if errorlevel 1 ( echo [ATTENTION] EN: FAILED ) else ( echo EN: FINISHED )
 
 cd %root%/zh
-lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH-verbose.tex"
-lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH-verbose.tex"
-echo ZH-VERBOSE: FINISHED
+lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH-verbose.tex" > %silentfile%
+lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH-verbose.tex" > %silentfile%
+if errorlevel 1 ( echo [ATTENTION] ZH-verbose: FAILED ) else ( echo ZH-verbose: FINISHED )
 
 cd %root%/zh
-lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH.tex"
-lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH.tex"
-echo ZH: FINISHED
+lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH.tex" > %silentfile%
+lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH.tex" > %silentfile%
+if errorlevel 1 ( echo [ATTENTION] ZH: FAILED ) else ( echo ZH: FINISHED )
+

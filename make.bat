5a315a2ac27766b8f51e2ea4fa5caf6ba57e9553
Vopaaz <liyifan945@163.com>
2019-09-03 11:28:23 -0500

    Makefile ATTENTION more clear

--- make.bat
+++ make.bat
@@ -6,20 +6,20 @@ set silentfile=silent.aux
 cd %root%/en
 lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-EN-verbose.tex" > %silentfile%
 lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-EN-verbose.tex" > %silentfile%
-if errorlevel 1 ( echo [ATTENTION] EN-verbose: FAILED ) else ( echo EN-verbose: FINISHED )
+if errorlevel 1 ( echo [ATTENTION] EN-verbose: FAILED [ATTENTION] ) else ( echo EN-verbose: finished )
 
 cd %root%/en
 lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-EN.tex" > %silentfile%
 lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-EN.tex" > %silentfile%
-if errorlevel 1 ( echo [ATTENTION] EN: FAILED ) else ( echo EN: FINISHED )
+if errorlevel 1 ( echo [ATTENTION] EN: FAILED [ATTENTION] ) else ( echo EN: finished )
 
 cd %root%/zh
 lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH-verbose.tex" > %silentfile%
 lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH-verbose.tex" > %silentfile%
-if errorlevel 1 ( echo [ATTENTION] ZH-verbose: FAILED ) else ( echo ZH-verbose: FINISHED )
+if errorlevel 1 ( echo [ATTENTION] ZH-verbose: FAILED [ATTENTION] ) else ( echo ZH-verbose: finished )
 
 cd %root%/zh
 lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH.tex" > %silentfile%
 lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH.tex" > %silentfile%
-if errorlevel 1 ( echo [ATTENTION] ZH: FAILED ) else ( echo ZH: FINISHED )
+if errorlevel 1 ( echo [ATTENTION] ZH: FAILED [ATTENTION] ) else ( echo ZH: finished )
 

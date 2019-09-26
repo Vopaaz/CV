da25fde18b2dc8e5b6e0bd390580d90621dd55f7
Vopaaz <liyifan945@163.com>
2019-09-26 14:59:17 -0500

    Adjust format

--- site/src/components/GeneralSection.vue
+++ site/src/components/GeneralSection.vue
@@ -11,14 +11,26 @@
         bodyStyle="font-size:14pt; font-family:Calibri, Helvetica, Arial, sans-serif;"
       >
         <span v-if="exp[lang]['sub-position']">
-          <a-card-grid style="width:100%;" class="info-card">{{ exp[lang]['position'] }}</a-card-grid>
-          <a-card-grid style="width:56%;" class="info-card">{{ exp[lang]['sub-position'] }} &nbsp;</a-card-grid>
-          <a-card-grid style="width:22%" class="info-card">{{ exp['time'] }}</a-card-grid>
+          <a-card-grid
+            style="width:100%;"
+            class="info-card position-card"
+          >{{ exp[lang]['position'] }}</a-card-grid>
+          <a-card-grid
+            style="width:53%;"
+            class="info-card position-card"
+          >{{ exp[lang]['sub-position'] }} &nbsp;</a-card-grid>
+          <a-card-grid style="width:25%" class="info-card">{{ process_time(exp['time']) }}</a-card-grid>
           <a-card-grid style="width:22%;" class="info-card">{{ exp[lang]['location'] }}</a-card-grid>
         </span>
         <span v-else>
-          <a-card-grid style="width:56%;" class="info-card">{{ exp[lang]['position'] }}</a-card-grid>
-          <a-card-grid style="width:22%" class="info-card">{{ exp['time'] }}</a-card-grid>
+          <a-card-grid
+            style="width:53%;"
+            class="info-card position-card"
+          >{{ exp[lang]['position'] }}</a-card-grid>
+          <a-card-grid
+            style="width:25%"
+            class="info-card"
+          >{{ process_time(exp['time']) }}</a-card-grid>
           <a-card-grid style="width:22%;" class="info-card">{{ exp[lang]['location'] }}</a-card-grid>
         </span>
         <a-card-grid style="width:100%;">
@@ -49,6 +61,34 @@ export default {
           x.match(re)[1]
         }</a>`;
       });
+    },
+    process_time: function(str) {
+      function format_CN_date(d) {
+        return `${d.getFullYear()}年${d.getMonth()}月`;
+      }
+
+      var start = new Date(str.split("-")[0].trim());
+      var end = str.split("-")[1].trim();
+      if (this.lang == "en") {
+        var options = {
+          year: "numeric",
+          month: "short"
+        };
+        start = start.toLocaleDateString("en-US", options);
+        if (end == "now") {
+          end = "Now";
+        } else {
+          end = new Date(end).toLocaleDateString("en-US", options);
+        }
+      } else {
+        start = format_CN_date(start);
+        if (end == "now") {
+          end = "至今";
+        } else {
+          end = format_CN_date(new Date(end));
+        }
+      }
+      return `${start} - ${end}`;
     }
   },
   computed: {
@@ -84,8 +124,12 @@ export default {
 .info-card {
   vertical-align: middle;
   padding: 12px;
-  padding-left: 24px;
-  padding-right: 24px;
+  padding-left: 14px;
+  padding-right: 14px;
+}
+
+.position-card {
+  padding-left: 30px;
 }
 
 h2 {

59bc79c681894449be96d2f453aa565990bf5917
Vopaaz <liyifan945@163.com>
2019-10-20 22:17:22 -0500

    Split make and makesite

--- make.bat
+++ make.bat
@@ -30,17 +30,3 @@ lualatex -synctex=1 -interaction=nonstopmode --shell-escape "./CV-ZH.tex" > %sil
 if errorlevel 1 ( echo [ATTENTION] ZH: FAILED [ATTENTION] ) else ( echo ZH: finished )
 
 echo PDF making succeed
-
-echo Copying files to site directory
-cd %root%
-copy "experience.json" "site/src/assets/experience.json"
-copy "en\CV-EN-verbose.pdf" "site\public\pdf\CV-EN-verbose.pdf"
-copy "en\CV-EN.pdf" "site\public\pdf\CV-EN.pdf"
-copy "zh\CV-ZH-verbose.pdf" "site\public\pdf\CV-ZH-verbose.pdf"
-copy "zh\CV-ZH.pdf" "site\public\pdf\CV-ZH.pdf"
-
-echo files copied to site directory
-
-echo Deploy the website
-cd %root%/site
-call deploy.bat
---
+++ makesite.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set root=%cd%
+
+echo Copying files to site directory
+copy "experience.json" "site/src/assets/experience.json"
+copy "en\CV-EN-verbose.pdf" "site\public\pdf\CV-EN-verbose.pdf"
+copy "en\CV-EN.pdf" "site\public\pdf\CV-EN.pdf"
+copy "zh\CV-ZH-verbose.pdf" "site\public\pdf\CV-ZH-verbose.pdf"
+copy "zh\CV-ZH.pdf" "site\public\pdf\CV-ZH.pdf"
+
+echo files copied to site directory
+
+echo Deploy the website
+cd %root%/site
+call deploy.bat
